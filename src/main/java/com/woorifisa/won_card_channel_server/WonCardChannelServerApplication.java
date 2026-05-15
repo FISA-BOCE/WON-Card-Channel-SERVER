980f6b3239fc67a59a5cc2b9573c60b9af0ab736
Chae-Yu <chaeyu2004@naver.com>
2026-05-15 11:54:52 +0900

    Setting: Spring Boot 초기 세팅 설정

---
+++ src/main/java/com/woorifisa/won_card_channel_server/WonCardChannelServerApplication.java
@@ -0,0 +1,13 @@
+package com.woorifisa.won_card_channel_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WonCardChannelServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WonCardChannelServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/woorifisa/won_card_channel_server/WonCardChannelServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woorifisa.won_card_channel_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WonCardChannelServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

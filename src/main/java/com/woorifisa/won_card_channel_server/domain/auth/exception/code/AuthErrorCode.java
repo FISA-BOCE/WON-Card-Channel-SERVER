0f2300d07aa163d89e3743c58514bb58d5ace096
caminobelllo <alstpgml0812@gmail.com>
2026-05-20 17:40:48 +0900

    Refactor: 분류 공백 추가

--- src/main/java/com/woorifisa/won_card_channel_server/domain/auth/exception/code/AuthErrorCode.java
+++ src/main/java/com/woorifisa/won_card_channel_server/domain/auth/exception/code/AuthErrorCode.java
@@ -14,8 +14,10 @@ public enum AuthErrorCode implements ErrorCode {
     TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "AUTH_401_003", "토큰이 만료되었습니다."),
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "AUTH_401_004", "유효하지 않은 토큰입니다."),
     INVALID_CURRENT_PASSWORD(HttpStatus.UNAUTHORIZED, "AUTH_401_005", "비밀번호가 일치하지 않습니다."),
+
     FORBIDDEN(HttpStatus.FORBIDDEN, "AUTH_403_001", "해당 요청에 대한 권한이 없습니다."),
     WITHDRAWN_ACCOUNT(HttpStatus.FORBIDDEN, "AUTH_403_002", "탈퇴한 계정입니다."),
+
     ALREADY_WITHDRAWN(HttpStatus.CONFLICT, "AUTH_409_001", "이미 탈퇴 처리된 계정입니다."),
     DUPLICATE_PHONE_NUMBER(HttpStatus.CONFLICT, "AUTH_409_002", "이미 가입된 휴대폰 번호입니다.");
 

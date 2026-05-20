846e7104982a3c40e21491590f74a9baf263c834
caminobelllo <alstpgml0812@gmail.com>
2026-05-20 17:16:39 +0900

    Refactor: api 응답 형식 변경
    - ResponseEntity 적용

--- src/main/java/com/woorifisa/won_card_channel_server/domain/auth/api/AuthController.java
+++ src/main/java/com/woorifisa/won_card_channel_server/domain/auth/api/AuthController.java
@@ -14,6 +14,7 @@ import com.woorifisa.won_card_channel_server.global.security.AuthenticatedUser;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -30,29 +31,37 @@ public class AuthController {
 
     @Operation(summary = "회원가입", description = "회원가입 API입니다.    \n비밀번호는 8자 이상이어야 합니다.")
     @PostMapping("/signup")
-    public ApiResponse<RegisterUserResponse> createUserRegistration(@RequestBody RegisterUserRequest request) {
-        return ApiResponse.of(SuccessStatus.SIGNUP_SUCCESS, authService.registerUser(request));
+    public ResponseEntity<ApiResponse<RegisterUserResponse>> createUserRegistration(@RequestBody RegisterUserRequest request) {
+        return ResponseEntity
+                .status(SuccessStatus.SIGNUP_SUCCESS.getHttpStatus())
+                .body(ApiResponse.of(SuccessStatus.SIGNUP_SUCCESS, authService.registerUser(request)));
     }
 
     @Operation(summary = "로그인", description = "로그인 API입니다.")
     @PostMapping("/login")
-    public ApiResponse<CreateLoginResponse> createUserLogin(@RequestBody CreateLoginRequest request) {
-        return ApiResponse.of(SuccessStatus.LOGIN_SUCCESS, authService.authenticateUser(request));
+    public ResponseEntity<ApiResponse<CreateLoginResponse>> createUserLogin(@RequestBody CreateLoginRequest request) {
+        return ResponseEntity
+                .status(SuccessStatus.LOGIN_SUCCESS.getHttpStatus())
+                .body(ApiResponse.of(SuccessStatus.LOGIN_SUCCESS, authService.authenticateUser(request)));
     }
 
     @Operation(summary = "토큰 재발급", description = "토큰 재발급 API입니다.    \nRefresh Token을 통해 Access Token을 재발급합니다.")
     @PostMapping("/refresh")
-    public ApiResponse<CreateTokenReissueResponse> createTokenReissue(@RequestBody CreateTokenReissueRequest request) {
-        return ApiResponse.of(SuccessStatus.TOKEN_REISSUE_SUCCESS, authService.reissueToken(request));
+    public ResponseEntity<ApiResponse<CreateTokenReissueResponse>> createTokenReissue(@RequestBody CreateTokenReissueRequest request) {
+        return ResponseEntity
+                .status(SuccessStatus.TOKEN_REISSUE_SUCCESS.getHttpStatus())
+                .body(ApiResponse.of(SuccessStatus.TOKEN_REISSUE_SUCCESS, authService.reissueToken(request)));
     }
 
     @Operation(summary = "로그아웃", description = "로그아웃 API입니다. session을 삭제합니다.")
     @PostMapping("/logout")
-    public ApiResponse<Void> deleteUserLogout(
+    public ResponseEntity<ApiResponse<Void>> deleteUserLogout(
             @AuthenticationPrincipal AuthenticatedUser authenticatedUser,
             @RequestBody DeleteLogoutRequest request
     ) {
         authService.logoutUser(authenticatedUser, request);
-        return ApiResponse.of(SuccessStatus.LOGOUT_SUCCESS);
+        return ResponseEntity
+                .status(SuccessStatus.LOGOUT_SUCCESS.getHttpStatus())
+                .body(ApiResponse.of(SuccessStatus.LOGOUT_SUCCESS));
     }
 }

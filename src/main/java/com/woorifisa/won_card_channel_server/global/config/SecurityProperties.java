a1d82a258ee1cd68bd557297ecc0ed5e274db14e
caminobelllo <alstpgml0812@gmail.com>
2026-05-20 15:47:05 +0900

    Fix: 검증 어노테이션 추가

--- src/main/java/com/woorifisa/won_card_channel_server/global/config/SecurityProperties.java
+++ src/main/java/com/woorifisa/won_card_channel_server/global/config/SecurityProperties.java
@@ -1,15 +1,27 @@
 package com.woorifisa.won_card_channel_server.global.config;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
 @Getter
 @Setter
+@Validated
 @ConfigurationProperties(prefix = "app.security")
 public class SecurityProperties {
+
+    @NotBlank
     private String jwtSecret;
+
+    @NotBlank
     private String cryptoSecret;
+
+    @Positive
     private long accessTokenExpirationSeconds;
+
+    @Positive
     private long refreshTokenExpirationSeconds;
 }

f83e07f8ab8691b6d5dbc1d1dbf36f84152e35b3
caminobelllo <alstpgml0812@gmail.com>
2026-05-20 20:26:12 +0900

    Feat: user 관련 dto 정의

--- src/main/java/com/woorifisa/won_card_channel_server/domain/user/dto/DeleteUserRequest.java
+++
@@ -1,9 +0,0 @@
-package com.woorifisa.won_card_channel_server.domain.user.dto;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record DeleteUserRequest(
-        @NotBlank
-        String refreshToken
-) {
-}

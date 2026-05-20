2c43d168bc450759c50fd63c26e963a45692a0a6
caminobelllo <alstpgml0812@gmail.com>
2026-05-20 16:39:34 +0900

    Feat: AuthUser Repository 구현
    - 비관적 락 적용

---
+++ src/main/java/com/woorifisa/won_card_channel_server/domain/auth/repository/CardChnAuthUserRepository.java
@@ -0,0 +1,19 @@
+package com.woorifisa.won_card_channel_server.domain.auth.repository;
+
+import com.woorifisa.won_card_channel_server.domain.auth.model.CardChnAuthUser;
+import jakarta.persistence.LockModeType;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CardChnAuthUserRepository extends JpaRepository<CardChnAuthUser, String> {
+
+    Optional<CardChnAuthUser> findByUserUuid(String userUuid);
+
+    Optional<CardChnAuthUser> findByTelHash(String telHash);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select u from CardChnAuthUser u where u.telHash = :telHash")
+    Optional<CardChnAuthUser> findByTelHashForUpdate(String telHash);
+}

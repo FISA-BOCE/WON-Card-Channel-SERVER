987164757f7892ebb9202dcfe3ed8719c286fa00
Chae-Yu <chaeyu2004@naver.com>
2026-05-21 14:55:28 +0900

    Chore: 스웨거 어노테이션 추가

--- src/main/java/com/woorifisa/won_card_channel_server/domain/reward/api/RewardLedgerApi.java
+++ src/main/java/com/woorifisa/won_card_channel_server/domain/reward/api/RewardLedgerApi.java
@@ -5,6 +5,8 @@ import com.woorifisa.won_card_channel_server.domain.reward.service.RewardLedgerS
 import com.woorifisa.won_card_channel_server.global.response.ApiResponse;
 import com.woorifisa.won_card_channel_server.global.response.SuccessStatus;
 import com.woorifisa.won_card_channel_server.global.security.AuthenticatedUser;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -14,10 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "Reward", description = "자동 투자 리워드 관련 API")
 public class RewardLedgerApi {
 
     private final RewardLedgerService rewardLedgerService;
 
+    @Operation(summary = "자동 투자 리워드 목록 조회", description = "자동 투자 된 리워드 목록 조회 페이지에서 사용되는 API입니다.")
     @GetMapping("/api/cards/rewards/ledger")
     public ResponseEntity<ApiResponse<RewardLedgerResponse>> getRewardLedger(
             @AuthenticationPrincipal AuthenticatedUser authenticatedUser,

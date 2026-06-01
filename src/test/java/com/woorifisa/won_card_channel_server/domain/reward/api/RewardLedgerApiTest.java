0a8647971afe9d542ee818fcd11a59f7bd9d8ace
Sungjun24s <medires17@naver.com>
2026-06-01 15:49:52 +0900

    Fix: RewardLedgerApiTest 오류 수정

--- src/test/java/com/woorifisa/won_card_channel_server/domain/reward/api/RewardLedgerApiTest.java
+++ src/test/java/com/woorifisa/won_card_channel_server/domain/reward/api/RewardLedgerApiTest.java
@@ -33,7 +33,8 @@ import org.springframework.test.web.servlet.MockMvc;
 @WebMvcTest(RewardLedgerApi.class)
 @AutoConfigureMockMvc(addFilters = false)
 @TestPropertySource(properties = {
-        "internal.card-core.url=http://localhost:8082"
+        "internal.card-core.url=http://localhost:8082",
+        "internal.invest-core.url=http://localhost:8083"
 })
 class RewardLedgerApiTest {
 

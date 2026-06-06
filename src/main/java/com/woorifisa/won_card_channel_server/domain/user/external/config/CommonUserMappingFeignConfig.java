f29b9e0c1483183148003d1efd313f5b6e79552a
jongyeon0214 <kwhddus0214@naver.com>
2026-06-07 03:51:14 +0900

    Fix: Common WAS 내부 API 호출 시 인증 헤더 누락 수정

--- src/main/java/com/woorifisa/won_card_channel_server/domain/user/external/config/CommonUserMappingFeignConfig.java
+++ src/main/java/com/woorifisa/won_card_channel_server/domain/user/external/config/CommonUserMappingFeignConfig.java
@@ -1,8 +1,10 @@
 package com.woorifisa.won_card_channel_server.domain.user.external.config;
 
 import feign.Request;
+import feign.RequestInterceptor;
 import feign.Retryer;
 import java.util.concurrent.TimeUnit;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 
 public class CommonUserMappingFeignConfig {
@@ -24,5 +26,16 @@ public class CommonUserMappingFeignConfig {
     @Bean
     public Retryer commonUserMappingRetryer() {
         return Retryer.NEVER_RETRY;
+    }
+
+    @Bean
+    public RequestInterceptor internalAuthInterceptor(
+            @Value("${internal.channel.service-id}") String serviceId,
+            @Value("${internal.channel.api-key}") String apiKey
+    ) {
+        return template -> {
+            template.header("X-Service-ID", serviceId);
+            template.header("X-Internal-Api-Key", apiKey);
+        };
     }
 }

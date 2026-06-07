364623d1f04eeb1a051d972d2860931e8ae40ee1
chaeyu <chaeyu2004@naver.com>
2026-06-07 19:36:29 +0900

    Chore: 스웨거 명세서 헤더 수정

--- src/main/java/com/woorifisa/won_card_channel_server/global/config/SwaggerConfig.java
+++ src/main/java/com/woorifisa/won_card_channel_server/global/config/SwaggerConfig.java
@@ -12,6 +12,10 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SwaggerConfig {
 
+    private static final String JWT_SECURITY_SCHEME = "JWT";
+    private static final String SERVICE_ID_SECURITY_SCHEME = "SERVICE_ID";
+    private static final String INTERNAL_API_KEY_SECURITY_SCHEME = "INTERNAL_API_KEY";
+
     @Bean
     public OpenAPI openAPI() {
 
@@ -19,15 +23,21 @@ public class SwaggerConfig {
                 .description("카드 채널계 Swagger UI입니다.")
                 .version("0.0.1");
 
-        String securityScheme = "JWT_TOKEN";
-
-        SecurityRequirement securityRequirement = new SecurityRequirement().addList(securityScheme);
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(JWT_SECURITY_SCHEME);
 
         Components components = new Components()
-                .addSecuritySchemes(securityScheme, new SecurityScheme()
+                .addSecuritySchemes(JWT_SECURITY_SCHEME, new SecurityScheme()
                         .type(SecurityScheme.Type.HTTP)
-                        .scheme("Bearer")
-                        .bearerFormat("JWT"));
+                        .scheme("bearer")
+                        .bearerFormat("JWT"))
+                .addSecuritySchemes(SERVICE_ID_SECURITY_SCHEME, new SecurityScheme()
+                        .type(SecurityScheme.Type.APIKEY)
+                        .in(SecurityScheme.In.HEADER)
+                        .name("X-Service-ID"))
+                .addSecuritySchemes(INTERNAL_API_KEY_SECURITY_SCHEME, new SecurityScheme()
+                        .type(SecurityScheme.Type.APIKEY)
+                        .in(SecurityScheme.In.HEADER)
+                        .name("X-Internal-Api-Key"));
 
         return new OpenAPI()
                 .info(info)

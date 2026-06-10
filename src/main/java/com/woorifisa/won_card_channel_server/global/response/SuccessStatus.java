dd56a018b06d09115a85b5e7d61db69dfc5383e6
chaeyu <chaeyu2004@naver.com>
2026-06-10 17:22:38 +0900

    Chore: SuccessStatus 추가

--- src/main/java/com/woorifisa/won_card_channel_server/global/response/SuccessStatus.java
+++ src/main/java/com/woorifisa/won_card_channel_server/global/response/SuccessStatus.java
@@ -24,6 +24,8 @@ public enum SuccessStatus {
 
     SWEEP_REQUEST_CREATED(HttpStatus.CREATED, "SWEEP_201_001", "스윕 요청이 생성되었습니다."),
     SWEEP_REQUEST_BATCH_CREATED(HttpStatus.CREATED, "SWEEP_201_002", "자동 스윕 요청 배치가 완료되었습니다."),
+    ADMIN_SWEEP_REQUESTS_FOUND(HttpStatus.OK, "ADMIN_SWEEP_200_001", "관리자 스윕 요청 목록 조회가 완료되었습니다."),
+    ADMIN_SWEEP_REQUEST_DETAIL_FOUND(HttpStatus.OK, "ADMIN_SWEEP_200_002", "관리자 스윕 요청 상세 조회가 완료되었습니다."),
 
     
     // 자동투자
